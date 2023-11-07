f9c57df107714e2c93349d5618dede07493e996d
aybeedee <abdullahumer575@gmail.com>
2023-11-07 20:51:19 +0500

    feat: update favicon and site metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: 'Next.js and Supabase Starter Kit',
-  description: 'The fastest way to build apps with Next.js and Supabase',
+  title: 'Garage Sale',
+  description: 'Buy quality household products at cheap prices',
 }
 
 export default function RootLayout({
