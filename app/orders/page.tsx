b21a547f334f13763e743cdff14b90a5b270b5c8
aybeedee <abdullahumer575@gmail.com>
2024-09-01 11:40:29 +0500

    chore: set profile pages to under construction, remove unused imports

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,4 +1,4 @@
-import { headers, cookies } from "next/headers";
+import { cookies } from "next/headers";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 
@@ -15,8 +15,13 @@ export default async function Orders() {
 	}
 
 	return (
-		<div className="px-5 py-12 sm:px-0 sm:py-28 w-full flex justify-center bg-white/10 shadow-[inset_10px_-50px_94px_0_rgb(199,199,199,0.2)] backdrop-blur">
-			{user?.email} ORDERS
+		<div className="px-5 py-12 sm:px-0 sm:py-28 w-full flex flex-col items-center gap-2 justify-center bg-white/10 shadow-[inset_10px_-50px_94px_0_rgb(199,199,199,0.2)] backdrop-blur">
+			<h1 className="mt-4 text-2xl font-semibold text-gray-700 capitalize">
+				ORDERS
+			</h1>
+			<p className="text-lg">
+				Webpage is currently under construction. Thank you for your patience!
+			</p>
 		</div>
 	);
 }
